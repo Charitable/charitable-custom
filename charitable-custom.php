0f8f3bf3fb036ee0e93a425af178ddedd7f165de
Eric Daams <eric@164a.com>
2017-03-20 16:33:46 +0930

    Initial commit

---
+++ charitable-custom.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:         Charitable - Our Customizations
+ * Plugin URI:          
+ * Description:         A base plugin you can use to add code customizations for your site.
+ * Version:             1.0
+ * Author:              WP Charitable
+ * Author URI:          https://www.wpcharitable.com/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
+
